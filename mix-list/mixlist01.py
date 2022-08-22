8d96bade0448acc7aae16a594aabcd7fdb84898a
linado18 <ltd_1975@yahoo.com>
2022-08-22 21:08:22 +0000

    teest

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Create a list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#Write a line of code to return the IP address from out list.
+print("The first item in the list (IP): " + my_list[0] )
+
+#Write the port 5060
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#Return the last item in the list which is value "UP"
+print("The last item in the list (state): " + my_list[2] )
+
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+
